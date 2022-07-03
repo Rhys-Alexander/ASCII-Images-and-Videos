981f9414b3718e168f539fcaf4b0f0b2b374204a
Gallahad072 <gallahad072@gmail.com>
2022-07-03 14:59:19 +0100

    opject oriented

--- main.py
+++ main.py
@@ -2,42 +2,43 @@ import cv2
 import numpy as np
 import time
 
-CHARACTERS = (" ", ".", "-", "+", "*", "&", "#", "@")
+
+class Converter:
+    CHARACTERS = (" ", " ", ".", "-", "+", "*", "&", "#", "@")
+    MIN_ASPECT_CHARS = 50
+
+    def __init__(self, filename) -> None:
+        self.vid = cv2.VideoCapture(filename)
+        frame = self.vid.read()[1]
+        self.aspect = self.MIN_ASPECT_CHARS / min(frame.shape[:2])
+        self.ms_per_frame = 1 / self.vid.get(cv2.CAP_PROP_FPS)
+
+    def getCharacter(self, pixel):
+        intensity = pixel / 255
+        return self.CHARACTERS[round(intensity * len(self.CHARACTERS)) - 1]
+
+    def convertToTerminal(self):
+        gotime = time.time()
+        while self.vid.isOpened():
+            if time.time() >= gotime:
+                gotime += self.ms_per_frame
+                ret, frame = self.vid.read()
+                if ret == True:
+                    frame = cv2.resize(frame, (0, 0), fx=self.aspect, fy=self.aspect)
+                    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                    h = frame.shape[0]
+                    w = frame.shape[1]
+                    cols = []
+                    for y in range(h):
+                        row = ""
+                        for x in range(w):
+                            row += self.getCharacter(frame[y, x]) * 2
+                        cols.append(row)
+                    print(np.matrix(cols))
+                else:
+                    break
+        self.vid.release()
 
 
-def getCharacter(pixel):
-    intensity = pixel / 255
-    return CHARACTERS[round(intensity * len(CHARACTERS)) - 1]
-
-
-def getVidProps(vid):
-    _, frame = vid.read()
-    aspect = 50 / min(frame.shape[:2])
-    ms_per_frame = 1 / vid.get(cv2.CAP_PROP_FPS)
-    return aspect, ms_per_frame
-
-
-vid = cv2.VideoCapture("vid.mp4")
-aspect, ms_per_frame = getVidProps(vid)
-gotime = time.time()
-while vid.isOpened():
-    if time.time() >= gotime:
-        gotime += ms_per_frame
-        time.sleep(0.01)
-        ret, frame = vid.read()
-        if ret == True:
-            frame = cv2.resize(frame, (0, 0), fx=aspect, fy=aspect)
-            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            h = frame.shape[0]
-            w = frame.shape[1]
-            cols = []
-            for y in range(h):
-                row = ""
-                for x in range(w):
-                    row += getCharacter(frame[y, x]) * 2
-                cols.append(row)
-            print(np.matrix(cols))
-        else:
-            break
-vid.release()
-cv2.destroyAllWindows()
+c = Converter("vid.mp4")
+c.convertToTerminal()
