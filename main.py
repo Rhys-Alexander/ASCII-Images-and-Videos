466a01381ad982f05583baf02ee23e6285552eb1
Gallahad072 <gallahad072@gmail.com>
2022-07-11 00:54:41 +0100

    can display ascii images

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import cv2
 import time
 import sys
 import mimetypes
+import numpy as np
 
 
 class Converter:
@@ -18,14 +19,21 @@ class Converter:
 
         if self.isVid:
             self.file = cv2.VideoCapture(filename)
+            self.ms_per_frame = 1 / self.file.get(cv2.CAP_PROP_FPS)
             frame = self.file.read()[1]
         else:
             self.file = cv2.imread(filename)
             frame = self.file
 
+        self.og_h = frame.shape[0]
+        self.og_w = frame.shape[1]
         self.aspect = self.MIN_ASPECT_CHARS / min(frame.shape[:2])
         self.h = round(frame.shape[0] * self.aspect)
         self.w = round(frame.shape[1] * self.aspect)
+        self.ascii_frame = self.imgToAscii(frame)
+
+        if self.isVid:
+            self.ascii_frames = self.asciiVidGenerator()
 
     def getMediaType(self, filename):
         mimestart = mimetypes.guess_type(filename)[0]
@@ -58,19 +66,41 @@ class Converter:
             else:
                 break
 
-    def saveFile(self):
-        # TODO save text to img and videos
-        pass
+    def displayAscii(self):
+        x_increment = round(self.og_w / len(self.ascii_frame[0]))
+        y_increment = round(self.og_h / len(self.ascii_frame))
+        width = x_increment * len(self.ascii_frame[0])
+        height = y_increment * len(self.ascii_frame)
+
+        if self.isVid:
+            pass
+        else:
+            img = np.zeros((height, width, 3), np.uint8)
+            for i, row in enumerate(self.ascii_frame):
+                y = (i + 1) * y_increment
+                for j, char in enumerate(row):
+                    x = j * x_increment
+                    cv2.putText(
+                        img=img,
+                        text=char,
+                        org=(x, y),
+                        fontFace=cv2.FONT_HERSHEY_PLAIN,
+                        fontScale=0.45,
+                        color=(0, 255, 0),
+                        thickness=1,
+                    )
+
+        cv2.imshow("test", img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
     def displayInTerminal(self):
         if self.isVid:
-            ms_per_frame = 1 / self.file.get(cv2.CAP_PROP_FPS)
-            ascii_frames = self.asciiVidGenerator()
             gotime = time.time()
-            for ascii_frame in ascii_frames:
+            for ascii_frame in self.ascii_frames:
                 while True:
                     if time.time() >= gotime:
-                        gotime += ms_per_frame
+                        gotime += self.ms_per_frame
                         for row in ascii_frame:
                             print(row)
                         for _ in range(len(ascii_frame)):
@@ -78,15 +108,15 @@ class Converter:
                             sys.stdout.write("\x1b[2K")  # delete the last line
                         break
         else:
-            ascii_frame = self.imgToAscii(self.file)
-            for row in ascii_frame:
+            for row in self.ascii_frame:
                 print(row)
             input()
-            for _ in range(len(ascii_frame) + 1):
+            for _ in range(len(self.ascii_frame) + 1):
                 sys.stdout.write("\x1b[1A")  # cursor up one line
                 sys.stdout.write("\x1b[2K")  # delete the last line
 
 
 if __name__ == "__main__":
-    c = Converter("vid.mp4")
+    c = Converter("pic.png")
     c.displayInTerminal()
+    c.displayAscii()
